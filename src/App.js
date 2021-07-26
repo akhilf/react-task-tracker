d4fadf6d0260969e3c99fafa7e777d11e3e561ad
Akhil Francis <akfranci@cisco.com>
2021-07-26 13:06:07 +0530

    Reminder functionality tasks

--- src/App.js
+++ src/App.js
@@ -10,19 +10,22 @@ function App() {
       id: "id1",
       name: "name1",
       day: "Feb 5th at 2.30 pm",
-      text: "doctor appointment"
+      text: "doctor appointment",
+      reminder: false
     },
     {
       id: "id2",
       name: "name2",
       day: "Feb 6th at 1.30 pm",
-      text: "Meeting at school"
+      text: "Meeting at school",
+      reminder: false
     },
     {
       id: "id3",
       name: "name3",
       day: "Feb 5th at 1.30 pm",
-      text: "Food shopping"
+      text: "Food shopping",
+      reminder: false
     }
   ])
 
@@ -30,10 +33,13 @@ function App() {
     setTasks(tasks.filter( (task) => task.id !== id))
   }
 
+  const onToggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder}: task));
+  }
   return (
     <div className="container">
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete = {deleteTask} /> : 'No Tasks to show'}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete = {deleteTask} onToggle = {onToggleReminder} /> : 'No Tasks to show'}
     </div>
   );
 }
